d5b66cf260e5ab633a62c60e435cb6c940a3da9c
PatrickEinstein <mohammedola1234@gmail.com>
2023-04-20 17:05:58 -0400

    rs

--- src/videoapp/socketcontex.js
+++ src/videoapp/socketcontex.js
@@ -6,7 +6,7 @@ import Peer from "simple-peer";
 const SocketContext = createContext();
 
 // const socket = io("http://localhost:5000");
-const socket = io("https://vcb-production.up.railway.app");
+const socket = io("https://vcb-production.up.railway.app/");
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
