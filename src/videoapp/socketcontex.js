6bcc6494b5ce98d8f20e34f4fdc52bfb1ff86095
PatrickEinstein <mohammedola1234@gmail.com>
2023-04-20 16:43:18 -0400

    remote backend

--- src/videoapp/socketcontex.js
+++ src/videoapp/socketcontex.js
@@ -5,7 +5,8 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
+const socket = io("https://vcb-production.up.railway.app");
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
